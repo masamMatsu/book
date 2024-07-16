1797d3f1efbf11736db6dc1828289229359c118c
masamMatsu <masaya.matsumiya@mail.o-hara.ac.jp>
2024-07-16 12:21:52 +0900

    初めてのコミット

---
+++ src/dao/DAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
+public class DAO {
+	static DataSource ds;
+
+	public Connection getConnection() throws Exception {
+		if (ds==null) {
+			//InitialContext ic=new InitialContext();
+			//ds=(DataSource)ic.lookup("java:/comp/env/jdbc/book");
+			Class.forName("org.h2.Driver");
+			DriverManager.getConnection("jdbc:h2:tcp://localhost/~/book", "sa", "");
+		}
+		Class.forName("org.h2.Driver");
+		return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/book", "sa", "");
+		//return ds.getConnection();
+	}
+}
